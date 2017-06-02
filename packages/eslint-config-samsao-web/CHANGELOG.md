92588bb6a75c7d57205be44f3498addf21877494
Matthieu Vachon <mvachon@samsao.co>
2017-06-02 10:24:48 -0400

    [web] [fix] Set `react/jsx-wrap-multilines` to `off` by default

--- packages/eslint-config-samsao-web/CHANGELOG.md
+++ packages/eslint-config-samsao-web/CHANGELOG.md
@@ -1,3 +1,13 @@
+## In progress
+
+#### Fix
+
+ * Set `react/jsx-wrap-multilines` to `off` by default.
+
+#### Support
+
+ * Removed `web.js` file entry.
+
 ## 1.0.0 (June 2, 2017)
 
  * Initial release.
--- packages/eslint-config-samsao-web/rules/react.js
+++ packages/eslint-config-samsao-web/rules/react.js
@@ -5,6 +5,7 @@ module.exports = {
     'react/jsx-boolean-value': ['error', 'always'],
     'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
     'react/jsx-indent-props': ['error', 2],
+    'react/jsx-wrap-multilines': 'off',
     'react/prefer-stateless-function': ['error', { ignorePureComponents: true }],
     'react/sort-comp': 'off',
   },
